fad85ed7d7d25fbacfe0b6573a13a23c2f397071
kw7oe <choongkwern@hotmail.com>
2018-11-20 12:23:00 +0800

    Complete exercise 2.71 and Skip exercise 2.72

---
+++ ch-2/ex-2-71.exs
@@ -0,0 +1,31 @@
+# For n = 5:
+#
+# Frequencies:
+# 1, 2, 4, 8, 16
+#
+# Tree:
+#         *
+#       /   \
+#      16    * (15)
+#          /   \
+#         8     * (7)
+#             /   \
+#            4     * (3)
+#                 / \
+#                1   2
+#
+# Bits for most frequency symbol: 4
+# Bits for least frequency symbol: 1
+#
+# For n = 10:
+#
+# Frequencies:
+# 1, 2, 4, 8, 16, 32, 64, 128, 256, 512
+#
+# Bits for most frequency symbol: 9
+# Bits for least frequency symbol: 1
+#
+# General:
+#
+# Bits for most frequency symbol: n - 1
+# Bits for least frequency symbol: 1
---
+++ ch-2/ex-2-72.exs
@@ -0,0 +1,3 @@
+# What is the order of growth in steps needed to encode a symbol?
+#
+# SKIP
